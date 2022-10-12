295be853ab1e10d30d01d342b55b6453930ff930
Jrod7938 <jrod7938@gmail.com>
2022-10-12 18:04:50 -0400

    completed HW1 for CSC17

---
+++ HW1/src/Document.java
@@ -0,0 +1,158 @@
+/** 
+ * A class that represents a text document
+ */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class Document {
+
+	private String text;
+	
+	/** Create a new document from the given text.
+	 * Because this class is abstract, this is used only from subclasses.
+	 * @param text The text of the document.
+	 */
+	protected Document(String text)
+	{
+		this.text = text;
+	}
+	
+	/** Returns the tokens that match the regex pattern from the document 
+	 * text string.
+	 * @param pattern A regular expression string specifying the 
+	 *   token pattern desired
+	 * @return A List of tokens from the document text that match the regex 
+	 *   pattern
+	 */
+	protected List<String> getTokens(String pattern)
+	{
+		ArrayList<String> tokens = new ArrayList<String>();
+		Pattern tokSplitter = Pattern.compile(pattern);
+		Matcher m = tokSplitter.matcher(text);
+		
+		while (m.find()) {
+			tokens.add(m.group());
+		}
+		
+		return tokens;
+	}
+	
+	/** This is a helper function that returns the number of syllables
+	 * in a word.  You should write this and use it in your 
+	 * BasicDocument class.
+     *
+	 * Just use a loop to loop through the 
+	 * characters in the string and write your own logic for counting 
+	 * syllables.
+	 * 
+	 * @param word  The word to count the syllables in
+	 * @return The number of syllables in the given word, according to 
+	 * this rule: Each contiguous sequence of one or more vowels is a syllable, 
+	 *       with the following exception: a lone "e" at the end of a word 
+	 *       is not considered a syllable unless the word has no other syllables. 
+	 *       You should consider y a vowel.
+	 */
+	protected int countSyllables(String word)
+	{
+	    // getNumSyllables method in BasicDocument
+        int numSyllables = 0;
+		boolean newSyllable = true;
+		String vowels = "aeiouy";
+		char[] cArray = word.toCharArray();
+		for (int i = 0; i < cArray.length; i++) {
+		// handling if there are repeating "e" or "e" at the end of a word
+		if (i == cArray.length-1 && Character.toLowerCase(cArray[i]) == 'e' && newSyllable && numSyllables > 0) {
+			numSyllables--;
+		}
+		// if the syllable's character is a vowel. Then it stops and count as a syllable.
+		if (newSyllable && vowels.indexOf(Character.toLowerCase(cArray[i])) >= 0) {
+			newSyllable = false;
+			numSyllables++;
+		} else if (vowels.indexOf(Character.toLowerCase(cArray[i])) < 0) { // if the current character is NOT a vowel, starts the new syllable. 
+			newSyllable = true;
+			}
+		}
+		return numSyllables;
+	}
+	
+	/** A method for testing
+	 * 
+	 * @param doc The Document object to test
+	 * @param syllables The expected number of syllables
+	 * @param words The expected number of words
+	 * @param sentences The expected number of sentences
+	 * @return true if the test case passed.  False otherwise.
+	 */
+	public static boolean testCase(Document doc, int syllables, int words, int sentences)
+	{
+		System.out.println("Testing text: ");
+		System.out.print(doc.getText() + "\n....");
+		boolean passed = true;
+		int syllFound = doc.getNumSyllables();
+		int wordsFound = doc.getNumWords();
+		int sentFound = doc.getNumSentences();
+		if (syllFound != syllables) {
+			System.out.println("\nIncorrect number of syllables.  Found " + syllFound 
+					+ ", expected " + syllables);
+			passed = false;
+		}
+		if (wordsFound != words) {
+			System.out.println("\nIncorrect number of words.  Found " + wordsFound 
+					+ ", expected " + words);
+			passed = false;
+		}
+		if (sentFound != sentences) {
+			System.out.println("\nIncorrect number of sentences.  Found " + sentFound 
+					+ ", expected " + sentences);
+			passed = false;
+		}
+		
+		if (passed) {
+			System.out.println("passed.\n");
+		}
+		else {
+			System.out.println("FAILED.\n");
+		}
+		
+		System.out.println(doc.getFleschScore());
+		
+		return passed;
+	}
+	
+	
+	/** Return the number of words in this document */
+	public abstract int getNumWords();
+	
+	/** Return the number of sentences in this document */
+	public abstract int getNumSentences();
+	
+	/** Return the number of syllables in this document */
+	public abstract int getNumSyllables();
+	
+	/** Return the entire text of this document */
+	public String getText()
+	{
+		return this.text;
+	}
+	
+	/** return the Flesch readability score of this document */
+	public double getFleschScore()throws ArithmeticException{
+		double fleschScore = 0;
+		// checking to see if the denominator is zero
+		try{
+			double wordBySentence = getNumWords()/getNumSentences();
+			double syllByWords = getNumSyllables()/getNumWords();
+
+			fleschScore = 206.835 - 1.015 * wordBySentence - 84.6 * syllByWords;
+		} catch(Exception e){
+			System.out.println("Arithmetic error, cannot devide by 0." + e);
+		}
+			
+	    return fleschScore;
+	}
+	
+	
+	
+}
