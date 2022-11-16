1f6652cd545861e8340bd897740bf1089a79bef1
Jrod7938 <jrod7938@gmail.com>
2022-11-16 14:21:30 -0500

    Implemented averageOfLevels() meathod

---
+++ CSC17Code/src/BinaryTree1.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class TreeNode {
+	public int key;
+	public TreeNode left, right;
+
+	public TreeNode(int item) {
+		key = item;
+		left = right = null;
+	}
+}
+
+public class BinaryTree1 {
+
+	public static List<String> binaryTreePaths(TreeNode node) {
+        // TODO: Place your code here
+
+        return null;
+	}
+
+    public static List<Double> averageOfLevels(TreeNode node) {
+        double total = 0.0;
+        double count = 0.0;
+		Queue<TreeNode> queue = new LinkedList<>();
+        List<Double> answer = new LinkedList<>();
+        queue.add(node);
+        // While root is not null
+        while(!queue.isEmpty()){
+            total = 0;
+            count = 0.0;
+
+            Queue<TreeNode> temp = new LinkedList<>();
+            // Add value to total, add left and right child to queue
+            while(!queue.isEmpty()){
+                TreeNode n = queue.peek();
+                queue.remove();
+                total += n.key;
+                count++;
+                if(n.left != null){ temp.add(n.left); }
+                if(n.right != null){ temp.add(n.right); }
+            }
+            queue = temp;
+            // add answer two our linked list
+            answer.add((total/count));
+        }
+        return answer;
+	}
+
+	public static void main(String args[]) {
+		/*
+		 *      4   <-- root
+		 *     / \
+		 *    3   1
+		 *       / \
+		 *      2   2
+		 *     / \ / \
+		 *    3  4 4  3
+		 */
+
+		TreeNode root = new TreeNode(4);
+		root.left = new TreeNode(3);
+		root.right = new TreeNode(1);
+		root.right.left = new TreeNode(2);
+		root.right.right = new TreeNode(2);
+		root.right.left.left = new TreeNode(3);
+		root.right.left.right = new TreeNode(4);
+		root.right.right.left = new TreeNode(4);
+		root.right.right.right = new TreeNode(3);
+
+		System.out.println(binaryTreePaths(root));
+		System.out.println(averageOfLevels(root));
+
+	}
+}
