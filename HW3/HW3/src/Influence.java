ff395d4d54b68237ac05937337512c4f9f295add
Jrod7938 <jrod7938@gmail.com>
2022-12-09 23:51:11 -0500

    Completed Influence.java and CC.java

--- HW3/HW3/src/Influence.java
+++ HW3/HW3/src/Influence.java
@@ -1,14 +1,16 @@
 import java.util.*;
 
 public class Influence {
+	public List<influencer> listofInfluencers;
+
 	// the nested class used to define a influencer
-	public static class influencer {
+	public static class influencer implements Comparable<influencer>{
 		int source; // the influencer
 		double power; // the impact of this influencer
 
 		influencer(int i, double p) {
-			source = i;
-			power = p;
+		source = i;
+		power = p;
 		}
 
 		int getSource() {
@@ -18,36 +20,69 @@ public class Influence {
 		double getPower() {
 			return power;
 		}
+
+		@Override // override influencer to compare for sorting
+		public int compareTo(influencer o) {
+			return (int) ((int) -1*(this.getPower() - o.getPower()));
+		}
 	}
 
-	private List<influencer> influencers;
-	
 	public Influence(Graph g) {
-		// this is the constructor to build the data structure that stores all influencers from graph g in a sorted order based on their influences.
-		
-		influencers = new ArrayList<>();
-		
-		for (int v : g.getVertices()) {
-			influencers.add(new influencer(v, influence(g, v)));
-		}
-		
-		// sort the influencers in descending order of power
-		Collections.sort(influencers, new Comparator<influencer>() {
-			@Override
-			public int compare(influencer i1, influencer i2) {
-				return Double.compare(i2.power, i1.power);
-			}
-		});
+	// place your code here
+	listofInfluencers = new ArrayList<>();
+	Set<Integer> vertices = g.getVertices();
+
+	for(Integer i: vertices){
+		int placeHolderSource = i;
+		double power = getPower(g, placeHolderSource);
+		influencer data = new influencer(placeHolderSource, power);
+
+		listofInfluencers.add(data);
 	}
-	
-	// Compute the influence of a given vertex
-	private double influence(Graph g, int v) {
-		// Placeholder implementation that simply returns the degree of the vertex
-		return g.adj(v).size();
+	// sort by power
+	Collections.sort(listofInfluencers);
 	}
 
 	public List<influencer> top(int k) {
-		// this is the method to return the top k influencers. The nested class influencer is provided to define an influencer.
-		return influencers.subList(0, Math.min(k, influencers.size()));
+		// place your code here
+		List<influencer> topK = new ArrayList<>();
+		for(int i = 0; i < k; i++){
+			topK.add(listofInfluencers.get(i));
+		}
+		return topK;
+	}
+
+	// get power 
+	public double getPower(Graph g, int source){
+		double power = 0;
+		HashMap<Integer, Integer> distTo = new HashMap<Integer, Integer>();
+		LinkedList<Integer> queue = new LinkedList<>();
+		// visited vertices
+		HashSet<Integer> visited = new HashSet<Integer>();
+
+		queue.addLast(source);
+		distTo.put(source, 0);
+
+		while(!queue.isEmpty()){
+			int vertice = queue.remove();
+			visited.add(vertice);
+
+			if(vertice != source){
+				power += 1 / Math.pow(2, (distTo.get(vertice) - 1)); // update power 
+			}
+
+			// go through the list 
+			Iterator<Integer> iterator = g.adj(vertice).iterator();
+			while(iterator.hasNext()){
+				int neighbor = iterator.next();
+
+				if(!visited.contains(neighbor)){
+					visited.add(neighbor);
+					queue.addLast(neighbor);
+					distTo.put(neighbor, distTo.get(vertice) + 1);
+				}
+			}
+		}
+		return power;
 	}
 }
